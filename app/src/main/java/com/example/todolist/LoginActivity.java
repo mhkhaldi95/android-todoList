e146e2a5fbfc733fc948a6ff58bd5c65f6769da6
EngMo974 <42417845+EngMo974@users.noreply.github.com>
2020-12-28 21:30:54 +0200

    design sign up and login screens connect login and sign up with firebase

--- app/src/main/java/com/example/todolist/LoginActivity.java
+++ app/src/main/java/com/example/todolist/LoginActivity.java
@@ -2,16 +2,69 @@ package com.example.todolist;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private Button login;
+    private EditText emailid, passwordd;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        mAuth = FirebaseAuth.getInstance();
+        emailid = findViewById(R.id.LoginEmail);
+        passwordd = findViewById(R.id.LoginPassword);
+        login = findViewById(R.id.Login_btn);
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailid.getText().toString();
+                String password = passwordd.getText().toString();
+                if (email.isEmpty()) {
+                    emailid.setError("Enter Your Email");
+                    emailid.requestFocus();
+                } else if (password.isEmpty()) {
+                    passwordd.setError("Enter Your Password");
+                    emailid.requestFocus();
+                } else if (email.isEmpty() && password.isEmpty()) {
+                    passwordd.setError("Enter Your Password");
+                    Toast.makeText(LoginActivity.this, "Fields are empty", Toast.LENGTH_SHORT).show();
+                } else if (!(email.isEmpty() && password.isEmpty())) {
+                    mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(LoginActivity.this, "Can't Login", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                                startActivity(intent);
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(LoginActivity.this, "Error Occurred!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         TextView tv_register = findViewById(R.id.To_SignUp);
         tv_register.setOnClickListener(view -> {
--- app/src/main/java/com/example/todolist/SignUp.java
+++ app/src/main/java/com/example/todolist/SignUp.java
@@ -2,16 +2,71 @@ package com.example.todolist;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SignUp extends AppCompatActivity {
+
+    private Button register;
+    private EditText emailid, passwordd;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up);
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+        emailid = findViewById(R.id.RegisterEmail);
+        passwordd = findViewById(R.id.RegisterPassword);
+        register = (Button) findViewById(R.id.Register_btn);
+        register.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View view) {
+                                            String email = emailid.getText().toString();
+                                            String password = passwordd.getText().toString();
+                                            if (email.isEmpty()) {
+                                                emailid.setError("Enter Your Email");
+                                                emailid.requestFocus();
+                                            } else if (password.isEmpty()) {
+                                                passwordd.setError("Enter Your Password");
+                                                emailid.requestFocus();
+                                            } else if (email.isEmpty() && password.isEmpty()) {
+                                                passwordd.setError("Enter Your Password");
+                                                Toast.makeText(SignUp.this, "Fields are empty", Toast.LENGTH_SHORT).show();
+                                            } else if (!(email.isEmpty() && password.isEmpty())) {
+                                                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(SignUp.this, new OnCompleteListener<AuthResult>() {
+                                                    @Override
+                                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                                        if (!task.isSuccessful()) {
+                                                            Toast.makeText(SignUp.this, "Error in Sign Up Please try again", Toast.LENGTH_SHORT).show();
+                                                        } else {
+                                                            startActivity(new Intent(SignUp.this, HomeActivity.class));
+                                                        }
+                                                    }
+                                                });
+                                            } else {
+                                                Toast.makeText(SignUp.this, "Error Occurred!", Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+                                    }
+
+
+        );
 
 
         TextView tv_register = findViewById(R.id.To_login);
